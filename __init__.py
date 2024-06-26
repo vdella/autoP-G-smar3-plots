862a40c617ada2c5475d7ff992cd2c6edc9cb83b
vdella <vitor.origamer@gmail.com>
2024-06-26 15:51:13 -0300

    renamed resources

--- __init__.py
+++ __init__.py
@@ -7,21 +7,24 @@ def data_from(filename):
 
 
 # Standard results for showing at front page.
-standard_level_file_data = data_from('nivel-tq2.txt')
-standard_sp_file_data = data_from('sp-tq2.txt')
-standard_valve_file_data = data_from('valve-tq2.txt')
+standard_level_file_data = data_from('standard/tank-level.txt')
+standard_sp_file_data = data_from('standard/sp.txt')
+standard_valve_file_data = data_from('standard/tank-valve.txt')
 
 # Adaptive gain comparison results.
-ad_gain_level_file_data = data_from('AD-GAIN-lvl-tank2.txt')
-ad_gain_sp_file_data = data_from('AD-GAIN-sp-tank2.txt')
-ad_gain_valve_file_data = data_from('AD-GAIN-ab-valve.txt')
+ad_gain_level_file_data = data_from('ad_gain/tank-level.txt')
+ad_gain_sp_file_data = data_from('ad_gain/sp.txt')
+ad_gain_valve_file_data = data_from('ad_gain/tank-valve.txt')
 
 # Anti-reset windup comparison results.
-arw_level_file_data = data_from('ARW-HI-lvl-tank2.txt')
-arw_sp_file_data = data_from('ARW-HI-sp-tank2.txt')
-arw_valve_file_data = data_from('ARW-HI-ab-valve.txt')
+arw_level_file_data = data_from('antireset_windup/tank-level.txt')
+arw_sp_file_data = data_from('antireset_windup/sp.txt')
+arw_valve_file_data = data_from('antireset_windup/tank-valve.txt')
 
 # Temperature test results
-measured_temperature_file_data = data_from('temp.txt')
-temperature_sp_file_data = data_from('temp-sp.txt')
+measured_temperature_file_data = data_from('temperature/temp.txt')
+temperature_sp_file_data = data_from('temperature/temp-sp.txt')
+tank2_level_temp_sim_file_data = data_from('temperature/tank-level.txt')
+tank2_valve_temp_sim_file_data = data_from('temperature/tank-valve.txt')
+tank2_sp_temp_sim_file_data = data_from('temperature/tank-sp.txt')
 
